df4db986dbfd0d06d53d4832effd19993b0a6b24
Darween1992 <69604886+Darween1992@users.noreply.github.com>
2021-10-16 11:27:08 -0500

    Add files via upload

---
+++ detalles_layly/includes/templates/header.php
@@ -0,0 +1,23 @@
+    <!--inicio del header-->
+    <header>
+        <div class="header-barra">
+            <h1>Detalles para toda ocacion</h1>
+        </div>
+        <div class="header-contenido">
+            <div class="header-contenido-logocom">
+                <a href="index.php"><img src="imagenes/LOGO1.jpg" alt="Logo Detalles LAYLY"></a>
+        </div>
+            <div class="header-contenido-barra">
+                <a href="tienda.php">Tienda</a>
+                <a href="acerca_de.php">Acerca de</a>
+                <a href="contacto.php">Contacto</a>
+                <a href="blog.php">Blog</a>
+            </div>
+            <div class="header-contenido-logos">
+                <a href="registro_usuarios.php"><img src="imagenes/iconos/login.png" alt="Registro de usuarios"></a>
+                <a href="login_admin.php"><img src="imagenes/iconos/admin-with-cogwheels.png" alt="Login Admin"></a>
+                <a href="carrito.php"><img src="imagenes/iconos/carritos-de-compra.png" alt="Carrito de compras"></a>
+            </div>
+        </div>
+    </header>
+    <!--final del header-->
---
+++ detalles_layly/tienda.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Detalles Layly</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<body>
+<?php
+    require 'includes/templates/header.php';
+?>   
+<h1 class="tienda-titulo">Nuestros Productos y Servicios</h1>
+<main>
+<div class="tienda-contenedor">
+    <div class="anuncio"><!--anuncio 1-->
+                    <div class="contenedor-imagen">
+                    <img src="imagenes/imagen1.jpeg" alt="ancheta 1">
+                    </div>
+                    <div class="contenido-anuncio">
+                        <h3>Ancheta de Rosas y Frutas</h3>
+                        <p>Ancheta con arreglo floral, frutas y foto de la pareja</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>
+                                <p>Arreglo floral</p>
+                            </li>
+                            <li>
+                                <p>Oso de peluche</p>
+                            </li>
+                            <li>
+                                <p>Dulces</p>
+                            </li>
+                        </ul>
+                        <p class="precio">$80.000</p>
+                        <div class="carrito">
+                        <input type="number" placeholder="Cantidad" id="cantidad">
+                        </div>
+                        <a href="#" class="boton-2">
+                                Agregar al Carrito
+                        </a>
+                    </div><!--contenido anuncio-->
+                </div><!--anuncio-->
+
+                <div class="anuncio"><!--anuncio 1-->
+                    <div class="contenedor-imagen">
+                    <img src="imagenes/imagen2.jpeg" alt="ancheta 1">
+                    </div>
+                    <div class="contenido-anuncio">
+                        <h3>Ancheta de Rosas y Frutas</h3>
+                        <p>Ancheta con arreglo floral, frutas y foto de la pareja</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>
+                                <p>Arreglo floral</p>
+                            </li>
+                            <li>
+                                <p>Oso de peluche</p>
+                            </li>
+                            <li>
+                                <p>Dulces</p>
+                            </li>
+                        </ul>
+                        <p class="precio">$80.000</p>
+                        <div class="carrito">
+                        <input type="number" placeholder="Cantidad" id="cantidad">
+                        </div>
+                        <a href="#" class="boton-2">
+                                Agregar al Carrito
+                        </a>
+                    </div><!--contenido anuncio-->
+                </div><!--anuncio-->
+
+                <div class="anuncio"><!--anuncio 1-->
+                    <div class="contenedor-imagen">
+                    <img src="imagenes/imagen3.jpeg" alt="ancheta 1">
+                    </div>
+                    <div class="contenido-anuncio">
+                        <h3>Ancheta de Rosas y Frutas</h3>
+                        <p>Ancheta con arreglo floral, frutas y foto de la pareja</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>
+                                <p>Arreglo floral</p>
+                            </li>
+                            <li>
+                                <p>Oso de peluche</p>
+                            </li>
+                            <li>
+                                <p>Dulces</p>
+                            </li>
+                        </ul>
+                        <p class="precio">$80.000</p>
+                        <div class="carrito">
+                        <input type="number" placeholder="Cantidad" id="cantidad">
+                        </div>
+                        <a href="#" class="boton-2">
+                                Agregar al Carrito
+                        </a>
+                    </div><!--contenido anuncio-->
+                </div><!--anuncio-->
+
+                <div class="anuncio"><!--anuncio 1-->
+                    <div class="contenedor-imagen">
+                    <img src="imagenes/imagen1.jpeg" alt="ancheta 1">
+                    </div>
+                    <div class="contenido-anuncio">
+                        <h3>Ancheta de Rosas y Frutas</h3>
+                        <p>Ancheta con arreglo floral, frutas y foto de la pareja</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>
+                                <p>Arreglo floral</p>
+                            </li>
+                            <li>
+                                <p>Oso de peluche</p>
+                            </li>
+                            <li>
+                                <p>Dulces</p>
+                            </li>
+                        </ul>
+                        <p class="precio">$80.000</p>
+                        <div class="carrito">
+                        <input type="number" placeholder="Cantidad" id="cantidad">
+                        </div>
+                        <a href="#" class="boton-2">
+                                Agregar al Carrito
+                        </a>
+                    </div><!--contenido anuncio-->
+                </div><!--anuncio-->
+                <div class="anuncio"><!--anuncio 1-->
+                    <div class="contenedor-imagen">
+                    <img src="imagenes/imagen5.jpg" alt="ancheta 1">
+                    </div>
+                    <div class="contenido-anuncio">
+                        <h3>Ancheta de Rosas y Frutas</h3>
+                        <p>Ancheta con arreglo floral, frutas y foto de la pareja</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>
+                                <p>Arreglo floral</p>
+                            </li>
+                            <li>
+                                <p>Oso de peluche</p>
+                            </li>
+                            <li>
+                                <p>Dulces</p>
+                            </li>
+                        </ul>
+                        <p class="precio">$80.000</p>
+                        <div class="carrito">
+                        <input type="number" placeholder="Cantidad" id="cantidad">
+                        </div>
+                        <a href="#" class="boton-2">
+                                Agregar al Carrito
+                        </a>
+                    </div><!--contenido anuncio-->
+                </div><!--anuncio-->
+</div>
+</main>
+
+<?php
+    require 'includes/templates/footer.php';
+?>   
+</body>
+</html>
